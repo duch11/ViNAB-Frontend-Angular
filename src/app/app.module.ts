c5dd1dd34fcb5ec4170c63a59bb180c6e7208045
Jonas Ladefoged Holm <jonas@holm.tech>
2018-11-12 09:12:53 -0500

    Login and register views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar';
 
-// material UI
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// bootstrap
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    RegisterUserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
